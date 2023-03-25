8879a85fc494c6d3228d9b4a8698ebfda954c1a3
Swarup <suryawanshiswarup@gamil.com>
2023-03-25 13:33:56 +0530

    create post button

--- src/components/PostsContainer/components/CreatePostCard.jsx
+++ src/components/PostsContainer/components/CreatePostCard.jsx
@@ -44,7 +44,10 @@ export default function CreatePostCard({}) {
       console.log(response?.data);
       setPosts((prev) => {
         const newState = [...prev];
-        newState.unshift(response?.data);
+        newState.unshift({
+          ...response?.data,
+          id: Number(newState?.length) + 1,
+        });
         return newState;
       });
 
@@ -61,7 +64,7 @@ export default function CreatePostCard({}) {
     setIsCreateMode(false);
   }
 
-  return (
+  return isCreateMode ? (
     <Card
       sx={{
         maxWidth: 345,
@@ -72,51 +75,77 @@ export default function CreatePostCard({}) {
         background: "cornsilk",
       }}
     >
-      {isCreateMode ? (
-        <>
-          <CardContent>
-            <Input
-              sx={{
-                fontSize: "1.2rem",
-                fontWeight: "bold",
-                width: "100%",
-              }}
-              value={formData?.title}
-              onChange={(e) => onChangeInput("title", e.target.value)}
-            />
-            <Input
-              sx={{
-                width: "100%",
-              }}
-              multiline
-              value={formData?.body}
-              onChange={(e) => onChangeInput("body", e.target.value)}
-            />
-          </CardContent>
-          <CardActions sx={{ justifyContent: "space-around" }}>
-            <Button
-              size="small"
-              startIcon={loading ? <Spinner /> : <AddCircleOutlineIcon />}
-              onClick={onClickCreate}
-              variant="contained"
-            >
-              Create
-            </Button>
-            <Button
-              size="small"
-              startIcon={<CancelIcon />}
-              onClick={onClickCancel}
-              variant="contained"
-            >
-              Cancel
-            </Button>
-          </CardActions>
-        </>
-      ) : (
-        <Box onClick={onClickAdd}>
-          <AddIcon />
-        </Box>
-      )}
+      <>
+        <CardContent>
+          <Input
+            sx={{
+              fontSize: "1.2rem",
+              fontWeight: "bold",
+              width: "100%",
+            }}
+            value={formData?.title}
+            onChange={(e) => onChangeInput("title", e.target.value)}
+          />
+          <Input
+            sx={{
+              width: "100%",
+            }}
+            multiline
+            value={formData?.body}
+            onChange={(e) => onChangeInput("body", e.target.value)}
+          />
+        </CardContent>
+        <CardActions sx={{ justifyContent: "space-around" }}>
+          <Button
+            size="small"
+            startIcon={loading ? <Spinner /> : <AddCircleOutlineIcon />}
+            onClick={onClickCreate}
+            variant="contained"
+          >
+            Create
+          </Button>
+          <Button
+            size="small"
+            startIcon={<CancelIcon />}
+            onClick={onClickCancel}
+            variant="contained"
+          >
+            Cancel
+          </Button>
+        </CardActions>
+      </>
+    </Card>
+  ) : (
+    <Card
+      sx={{
+        maxWidth: 345,
+        width: "100%",
+        height: 280,
+        m: 1,
+        p: 1,
+        background: "#565656",
+      }}
+    >
+      <Button
+        sx={{
+          width: "100%",
+          height: "100%",
+          background: "#565656",
+          border: "1px solid whitesmoke",
+          boxSizing: "border-box",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        onClick={onClickAdd}
+      >
+        <AddIcon
+          sx={{
+            fontSize: "3rem",
+            color: "whitesmoke",
+          }}
+        />
+      </Button>
     </Card>
   );
 }
